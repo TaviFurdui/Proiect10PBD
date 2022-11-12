dd63aaeca3aed3cce69ada419f86372518fc150b
TaviFurdui <58124870+TaviFurdui@users.noreply.github.com>
2022-11-12 22:38:22 +0200

    Cerintele 4, 5 si 6

--- Proiect10PBD/Form1.Designer.cs
+++ Proiect10PBD/Form1.Designer.cs
@@ -33,6 +33,9 @@ namespace Proiect10PBD
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonStudenti = new System.Windows.Forms.Button();
             this.buttonNote = new System.Windows.Forms.Button();
+            this.buttonNepromovati = new System.Windows.Forms.Button();
+            this.buttonRaport5 = new System.Windows.Forms.Button();
+            this.buttonRaport6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,10 +44,9 @@ namespace Proiect10PBD
             this.buttonMaterii.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buttonMaterii.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonMaterii.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.buttonMaterii.Location = new System.Drawing.Point(67, 139);
-            this.buttonMaterii.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonMaterii.Location = new System.Drawing.Point(50, 113);
             this.buttonMaterii.Name = "buttonMaterii";
-            this.buttonMaterii.Size = new System.Drawing.Size(290, 104);
+            this.buttonMaterii.Size = new System.Drawing.Size(218, 84);
             this.buttonMaterii.TabIndex = 0;
             this.buttonMaterii.Text = "Afisare materii";
             this.buttonMaterii.UseVisualStyleBackColor = false;
@@ -53,21 +55,19 @@ namespace Proiect10PBD
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(481, 136);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(361, 110);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(719, 421);
+            this.dataGridView1.Size = new System.Drawing.Size(539, 345);
             this.dataGridView1.TabIndex = 1;
             // 
             // buttonStudenti
             // 
             this.buttonStudenti.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buttonStudenti.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonStudenti.Location = new System.Drawing.Point(67, 304);
-            this.buttonStudenti.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonStudenti.Location = new System.Drawing.Point(50, 203);
             this.buttonStudenti.Name = "buttonStudenti";
-            this.buttonStudenti.Size = new System.Drawing.Size(290, 97);
+            this.buttonStudenti.Size = new System.Drawing.Size(218, 79);
             this.buttonStudenti.TabIndex = 2;
             this.buttonStudenti.Text = "Afisare studenti";
             this.buttonStudenti.UseVisualStyleBackColor = false;
@@ -77,25 +77,62 @@ namespace Proiect10PBD
             // 
             this.buttonNote.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buttonNote.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonNote.Location = new System.Drawing.Point(67, 454);
-            this.buttonNote.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonNote.Location = new System.Drawing.Point(50, 288);
             this.buttonNote.Name = "buttonNote";
-            this.buttonNote.Size = new System.Drawing.Size(290, 103);
+            this.buttonNote.Size = new System.Drawing.Size(218, 84);
             this.buttonNote.TabIndex = 3;
             this.buttonNote.Text = "Afisare note";
             this.buttonNote.UseVisualStyleBackColor = false;
             this.buttonNote.Click += new System.EventHandler(this.buttonNote_Click);
             // 
+            // buttonNepromovati
+            // 
+            this.buttonNepromovati.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonNepromovati.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNepromovati.Location = new System.Drawing.Point(50, 378);
+            this.buttonNepromovati.Name = "buttonNepromovati";
+            this.buttonNepromovati.Size = new System.Drawing.Size(218, 84);
+            this.buttonNepromovati.TabIndex = 4;
+            this.buttonNepromovati.Text = "Afisare studenti picati";
+            this.buttonNepromovati.UseVisualStyleBackColor = false;
+            this.buttonNepromovati.Click += new System.EventHandler(this.buttonNepromovati_Click);
+            // 
+            // buttonRaport5
+            // 
+            this.buttonRaport5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonRaport5.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRaport5.Location = new System.Drawing.Point(50, 468);
+            this.buttonRaport5.Name = "buttonRaport5";
+            this.buttonRaport5.Size = new System.Drawing.Size(218, 84);
+            this.buttonRaport5.TabIndex = 5;
+            this.buttonRaport5.Text = "Afisare raport studenti";
+            this.buttonRaport5.UseVisualStyleBackColor = false;
+            this.buttonRaport5.Click += new System.EventHandler(this.buttonRaport5_Click);
+            // 
+            // buttonRaport6
+            // 
+            this.buttonRaport6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonRaport6.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRaport6.Location = new System.Drawing.Point(50, 567);
+            this.buttonRaport6.Name = "buttonRaport6";
+            this.buttonRaport6.Size = new System.Drawing.Size(218, 84);
+            this.buttonRaport6.TabIndex = 6;
+            this.buttonRaport6.Text = "Afisare raport detaliat";
+            this.buttonRaport6.UseVisualStyleBackColor = false;
+            this.buttonRaport6.Click += new System.EventHandler(this.buttonRaport6_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1259, 705);
+            this.ClientSize = new System.Drawing.Size(944, 749);
+            this.Controls.Add(this.buttonRaport6);
+            this.Controls.Add(this.buttonRaport5);
+            this.Controls.Add(this.buttonNepromovati);
             this.Controls.Add(this.buttonNote);
             this.Controls.Add(this.buttonStudenti);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonMaterii);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -109,6 +146,9 @@ namespace Proiect10PBD
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonStudenti;
         private System.Windows.Forms.Button buttonNote;
+        private System.Windows.Forms.Button buttonNepromovati;
+        private System.Windows.Forms.Button buttonRaport5;
+        private System.Windows.Forms.Button buttonRaport6;
     }
 }
 
--- Proiect10PBD/Form1.cs
+++ Proiect10PBD/Form1.cs
@@ -54,5 +54,56 @@ namespace Proiect10PBD
             bSource.DataSource = dt;
             dataGridView1.DataSource = bSource;
         }
+
+        private void buttonNepromovati_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand(@"
+                SELECT studenti.nume, studenti.prenume, note.nota, note.id_materie FROM studenti
+                INNER JOIN note ON note.nr_legitimatie = studenti.nr_legitimatie
+                GROUP BY note.id_materie, note.nr_legitimatie
+                HAVING MAX(note.nota) < 5", DBConnexion.con);
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            BindingSource bSource = new BindingSource();
+            bSource.DataSource = dt;
+            dataGridView1.DataSource = bSource;
+        }
+
+        private void buttonRaport5_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand(@"
+                SELECT studenti.nume, studenti.prenume, MAX(materii.an_studiu) AS an_studiu, studenti.nr_legitimatie FROM studenti 
+                INNER JOIN note ON note.nr_legitimatie = studenti.nr_legitimatie 
+                INNER JOIN materii ON materii.id_materie = note.id_materie 
+                GROUP BY studenti.nr_legitimatie", DBConnexion.con);
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            BindingSource bSource = new BindingSource();
+            bSource.DataSource = dt;
+            dataGridView1.DataSource = bSource;
+        }
+
+        private void buttonRaport6_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand(@"
+               SELECT studenti.nume, studenti.prenume, studenti.nr_legitimatie, materii.nume_materie, MAX(note.nota) AS nota_finala FROM studenti
+                INNER JOIN note ON note.nr_legitimatie = studenti.nr_legitimatie
+                INNER JOIN materii ON materii.id_materie = note.id_materie 
+                GROUP BY note.id_materie, note.nr_legitimatie
+                HAVING MAX(note.nota) > 4
+                ORDER BY studenti.nume, studenti.prenume, materii.an_studiu, materii.nume_materie", DBConnexion.con);
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            BindingSource bSource = new BindingSource();
+            bSource.DataSource = dt;
+            dataGridView1.DataSource = bSource;
+        }
     }
+    
 }
